767d1d9a9e3cad325dd57af89c6b1055f9a94bcb
Kevin Qi <iqnivek@gmail.com>
2020-05-21 14:51:35 -0700

    pass data with callbacks, WIP lobbies

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
 
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 function App() {
+  const [socket, setSocket] = React.useState(null);
+
   React.useEffect(() => {
-    const socket = io.connect('http://localhost:3001');
-
-    socket.on('init', (item) => {
-      console.log('init', item);
-    });
-
-    socket.emit('join', 'ABCDEF');
+    const newSocket = io.connect("http://localhost:3001");
+    setSocket(newSocket);
   }, []);
 
   return (
     <div>
-      <h1>Setkemps</h1>
+      <h1>Anathema</h1>
+      {socket && (
+        <Button
+          variant="primary"
+          onClick={() => {
+            socket.emit("lobby:create", {}, (result) => {
+              console.log("lobby:create", result);
+            });
+          }}
+        >
+          Create lobby
+        </Button>
+      )}
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,20 +1,29 @@
-const server = require('http').createServer();
+const server = require("http").createServer();
 
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
 let state = {
   lobbies: {},
 };
 
-io.on('connection', (client) => {
-  console.log('Client connected:', client.id);
+io.on("connection", (client) => {
+  console.log("Client connected:", client.id);
 
-  client.on('join', (lobbyCode) => {
-    console.log(`[join] ${client.id} joining lobby ${lobbyCode}`);
-
-    client.emit('init', { cards: [] });
+  client.on("lobby:create", (data, callback) => {
+    console.log("lobby:create", data);
+    const lobbyCode = Math.random().toString();
+    const lobby = {
+      lobbyCode,
+      players: [client.id],
+    };
+    state.lobbies[lobbyCode] = lobby;
+    callback(lobby);
   });
-  
+
+  client.on("lobby:join", (lobbyCode) => {
+    console.log(`[join] ${client.id} joining lobby ${lobbyCode}`);
+  });
+
   // TODO: disconnect, error handlers
 });
 
